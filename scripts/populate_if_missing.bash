0a08dd636578cff28cdb46f1b9ee8d3d62c29d92
Sam Pearson <sam@sgp.me.uk>
2021-02-09 12:04:18 +0000

    Populate script: ensure there is some configuration to work with
    This also adjusts the `cd` command to be dependent on the location of
    the script, avoiding hardcoding a directory to `cd` to.

--- scripts/populate_if_missing.bash
+++ scripts/populate_if_missing.bash
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+set -e
+
+cd "$(dirname "$0")/.."
+
+if [ ! -f ./conf/config.py ] ; then
+    cp conf/config.py-example conf/config.py
+fi
+
 if [ ! -f /app/databases/db.sqlite3 ]; then
-    cd /app
     python manage.py migrate
     python manage.py populate
 fi
