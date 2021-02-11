869cc651fe74040534b90c22a0e231f9c7d5e232
Alex Parsons <alex@alexparsons.co.uk>
2021-02-11 15:16:32 +0000

    Removed vagrant and old method for upload

--- tasks.py
+++ tasks.py
@@ -16,17 +16,6 @@ try:
     os.environ.pop("DJANGO_SETTINGS_MODULE")
 except Exception:
     pass
-try:
-    from conf.ssh_creds import *  # move ssh creds into config
-except:
-    #server hosting static files
-    MYSOC_SERVER = ""
-    #key filename
-    SSH_FILENAME = ""
-    #if used, passphrase (use environ)
-    SSH_PASSPHRASE = ""
-    #destination (minus app name) on the server
-    DESTINATION_ROOT = ""
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proj.settings")
 
@@ -34,7 +23,6 @@ django.setup()
 
 app_name = settings.SITE_SLUG
 
-destination_path = DESTINATION_ROOT + app_name
 bake_dir = settings.BAKE_LOCATION + "/sites/" + app_name + "/"
 bake_zip = settings.BAKE_LOCATION + "/sites/" + app_name + ".zip"
 
@@ -78,25 +66,3 @@ def bakezip(c):
 @task
 def broadcast(c):
     do_django_command("runserver", "0.0.0.0:8000")
-
-
-@task
-def uploadzip(c):
-
-    c = Connection(MYSOC_SERVER,
-                   user=SSH_USER,
-                   port=22,
-                   connect_kwargs={"key_filename": SSH_FILENAME,
-                                   "passphrase": SSH_PASSPHRASE})
-    c.open()
-    path, filename = os.path.split(bake_zip)
-
-    sudo_password = ""
-    while not sudo_password:
-        sudo_password = getpass("sudo password: ")
-    print("uploading {0}".format(filename))
-    c.put(bake_zip, filename)
-    #c.sudo('rm -rf {0}'.format(destination_path))
-    c.sudo('unzip -o {0} -d {1}'.format(filename, destination_path),
-           password=sudo_password)
-    c.close()
