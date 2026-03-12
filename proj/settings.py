9a4e17310f3609f8dd14dbeb70d55a1463edb8cd
Alex Parsons <alex.parsons@mysociety.org>
2026-03-12 09:25:15 +0000

    django 5.2 update

--- proj/settings.py
+++ proj/settings.py
@@ -49,6 +49,7 @@ TEMPLATES = [
                 "proj.universal.universal_context",
                 "django.contrib.auth.context_processors.auth",
                 "django.template.context_processors.debug",
+                "django.template.context_processors.request",
                 "django.template.context_processors.i18n",
                 "django.template.context_processors.media",
                 "django.template.context_processors.static",
